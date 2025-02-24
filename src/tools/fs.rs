3eed3394cbc4f1d404d916d991328e8c074accc5
ALATEKA <apf@alateka.es>
2025-02-25 00:36:11 +0100

    feat: Add new function on FileIO struct to read files

--- src/tools/fs.rs
+++ src/tools/fs.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::Write;
 use std::io::Result;
+use std::path::Path;
 use std::fs::OpenOptions;
-
+use std::fs::read_to_string;
 
 pub struct FileIO<'a> {
     path:    &'a str
@@ -14,8 +15,12 @@ impl<'a> FileIO<'a> {
         Self { path }
     }
 
-    pub fn read(self) {
-        
+    pub fn read(self) -> String {
+        let mut content: String = String::from("");
+        if Path::new(self.path).exists() {
+            content = read_to_string(self.path).expect("Error in file");
+        }
+        content
     }
 
     pub fn write(self, content: String) -> Result<()> {
